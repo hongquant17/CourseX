9b77665c4b96025158d87c3175971ff466ced65d
Hong Quan <hongquant.1712@gmail.com>
2023-10-15 00:49:16 +0700

    feat: add authentication

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
+  )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return ( 
+        <div>
+            This should be unprotected
+        </div>
+     );
+}
+ 
+export default TestPage
