9f629876b7b0356a1e71d27826cd591022fe7243
pqtrung <phuquoctrung@2003@gmail.com>
2023-10-20 00:02:39 +0700

    [COURSE-DETAILS] link course page to database

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,16 +1,50 @@
+import { auth } from "@clerk/nextjs";
 
-const CourseIdPage = ({
-    params
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({
+  params
 }: {
-    params: { courseId: string }
-}
+  params: { courseId: string }
+}) => {
+  const { userId } = auth();
 
-) => {
-    return (
-        <div>
-            Course Id: {params.courseId}
-        </div>
-    );
+  if (!userId){
+    return redirect("/")
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  });
+
+  if (!course) {
+    return redirect('/')
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  const isComplete = requiredFields.every(Boolean);
+
+
+  return (
+    <div>
+      Course Id: {params.courseId}
+    </div>
+  )
 }
 
 export default CourseIdPage;
