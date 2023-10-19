5f12fc63bac7b6249c4ca28fad1ee5a160b00930
pqtrung <phuquoctrung@2003@gmail.com>
2023-10-20 00:15:30 +0700

    [COURSE-DETAILS] Course edit page UI

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { IconBadge } from "@/components/icon-badge";
+import { LayoutDashboard } from "lucide-react";
 
 const CourseIdPage = async ({
   params
@@ -41,8 +43,27 @@ const CourseIdPage = async ({
 
 
   return (
-    <div>
-      Course Id: {params.courseId}
+    <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">
+              Course setup
+            </h1>
+            <span className="text-sm text-slate-700">
+              Complete all fields {completionText}
+            </span>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+            <div>
+                <div className="flex items-center gap-x-2">
+                    <IconBadge icon={LayoutDashboard} />
+                    <h2 className="text-xl">
+                        Customize your course
+                    </h2>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
