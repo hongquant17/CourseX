9cdc56bfe7ba42ac786646d62a863209a6b1eb38
leson502 <leson50241@gmail.com>
2023-10-27 11:50:03 +0700

    [QLKHGV_EDIT] add cateogry form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { LayoutDashboard } from "lucide-react";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({
   params
@@ -16,13 +17,19 @@ const CourseIdPage = async ({
 }) => {
   const { userId } = auth();
 
-  if (!userId){
+  if (!userId) {
     return redirect("/")
   }
 
   const course = await db.course.findUnique({
     where: {
       id: params.courseId
+    },
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
     }
   });
 
@@ -48,38 +55,46 @@ const CourseIdPage = async ({
 
   return (
     <div className="p-6">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-y-2">
-            <h1 className="text-2xl font-medium">
-              Course setup
-            </h1>
-            <span className="text-sm text-slate-700">
-              Complete all fields {completionText}
-            </span>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">
+              Customize your course
+            </h2>
           </div>
+          <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <ImageForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            option={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-            <div>
-                <div className="flex items-center gap-x-2">
-                    <IconBadge icon={LayoutDashboard} />
-                    <h2 className="text-xl">
-                        Customize your course
-                    </h2>
-                </div>
-                <TitleForm
-                  initialData={course}
-                  courseId={course.id}
-                />
-                <DescriptionForm
-                  initialData={course}
-                  courseId={course.id}
-                />
-                <ImageForm
-                  initialData={course}
-                  courseId={course.id}
-                />
-            </div>
-        </div>
+      </div>
     </div>
   )
 }
