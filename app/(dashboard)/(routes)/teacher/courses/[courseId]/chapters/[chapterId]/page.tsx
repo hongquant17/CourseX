aaf6702c3159eb61be7c1f16a45daf12ed69ecdc
hongquant17 <hongquant.1712@gmail.com>
2023-11-02 21:05:40 +0700

    [QLCHGV_ADDCHAP] chapter description (temporary)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,84 +5,82 @@ import { db } from "@/lib/db";
 import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
-    params,
+  params,
 }: {
-    params: { courseId: string; chapterId: string };
+  params: { courseId: string; chapterId: string };
 }) => {
-    const { userId } = auth();
+  const { userId } = auth();
 
-    if (!userId) {
-        return redirect("/");
-    }
+  if (!userId) {
+    return redirect("/");
+  }
 
-    const chapter = await db.chapter.findUnique({
-        where: {
-            id: params.chapterId,
-            courseId: params.courseId,
-        },
-        include: {
-            muxData: true,
-        },
-    });
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true,
+    },
+  });
 
-    if (!chapter) {
-        return redirect("/");
-    }
+  if (!chapter) {
+    return redirect("/");
+  }
 
-    const requiredFields = [
-        chapter.title,
-        chapter.description,
-        chapter.videoUrl,
-    ];
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
 
-    const totalFields = requiredFields.length;
-    const completedFields = requiredFields.filter(Boolean).length;
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
 
-    const completionFields = `(${completedFields}/${totalFields})`;
+  const completionFields = `(${completedFields}/${totalFields})`;
 
-    return (
-        <div className="p-6">
-            <div className="flex items-center justify-between">
-                <div className="w-full">
-                    <Link
-                        href={`/teacher/courses/${params.courseId}`}
-                        className="flex items-center text-sm hover::opacity-75
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="w-full">
+          <Link
+            href={`/teacher/courses/${params.courseId}`}
+            className="flex items-center text-sm hover::opacity-75
                         transition mb-6"
-                    >
-                        <ArrowLeft className="h-4 w-4 mr-2" />
-                        Back to course set up
-                    </Link>
-                    <div className="flex items-center justify-between w-full">
-                        <div className="flex flex-col gap-y-2">
-                            <h1 className="text-2xl font-medium">
-                                Chapter Creation
-                            </h1>
-                            <span className="text-sm text-slate-700">
-                                Complete out all fields {completionFields}
-                            </span>
-                        </div>
-                    </div>
-                </div>
+          >
+            <ArrowLeft className="h-4 w-4 mr-2" />
+            Back to course set up
+          </Link>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex flex-col gap-y-2">
+              <h1 className="text-2xl font-medium">Chapter Creation</h1>
+              <span className="text-sm text-slate-700">
+                Complete out all fields {completionFields}
+              </span>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-                <div className="space-y-4">
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge icon={LayoutDashboard}/>
-                        <h2 className="text-xl">
-                            Cutomization
-                        </h2>
-                    </div>
-                    <ChapterTitleForm
-                        initialData={chapter}
-                        courseId={params.courseId}
-                        chapterId={params.chapterId}
-                    />
-                </div>
-            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="space-y-4">
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customization</h2>
+          </div>
+          <ChapterTitleForm
+            initialData={chapter}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+          />
+          <ChapterDescriptionForm
+            initialData={chapter}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChapterIdPage;
