08eda7017a4a3e0829b3e62d8e4afce7b599c698
hongquant17 <hongquant.1712@gmail.com>
2023-11-04 23:09:21 +0700

    [QLKHGV_ADD] add categories

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,15 +1,29 @@
-import { Database } from "lucide-react";
+// import { Database } from "lucide-react";
 
 const { PrismaClient } = require("@prisma/client");
 
 const database = new PrismaClient();
 
 async function main() {
-    try {
-        
-    } catch (error) {
-        console.log("Error while seeding the database categories", error);
-    } finally {
-        await database.$disconnect();
-    }
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Maths" },
+        { name: "Travel" },
+        { name: "Marketing" },
+        { name: "Engineering" },
+      ],
+    });
+
+    console.log("Success");
+
+  } catch (error) {
+    console.log("Error while seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
 }
+
+main();
