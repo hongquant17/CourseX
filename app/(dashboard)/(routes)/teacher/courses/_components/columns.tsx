da3bb68ac7565b6388b514c87aee97868b2a962a
xpbachx <21020283@vnu.edu.vn>
2023-11-06 15:40:40 +0700

    [QLKHGV_VIEW] Sort column

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -2,18 +2,47 @@
 
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({column}) => {
+        return (
+            <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+            Course name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
+    },
   },
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({column}) => {
+        return (
+            <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+            Price
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
+    },
   },
   {
     accessorKey: "isPublished",
-    header: "Published",
+    header: ({column}) => {
+        return (
+            <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+            Published
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
+    },
   },
 ]
--- app/(dashboard)/(routes)/teacher/courses/_components/data-table.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/data-table.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import * as React from "react";
 import {
   ColumnDef,
   flexRender,
+  SortingState,
   getCoreRowModel,
   useReactTable,
+  getSortedRowModel,
   getPaginationRowModel,
 } from "@tanstack/react-table"
 
@@ -27,11 +30,18 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   })
 
   return (
