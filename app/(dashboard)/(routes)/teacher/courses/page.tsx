760a38870df88183e3ba2fb4c1ed9c53ef2750e3
xpbachx <21020283@vnu.edu.vn>
2023-11-06 15:01:40 +0700

    [QLKHGV_VIEW] Data table UI for showing courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,26 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () =>{
+async function getData(): Promise<any[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+const CoursesPage = async() =>{
+    const data = await getData();
     return (
       <div className="h-full flex justify-center pt-20">
-        <Link href="/teacher/create">
-          <Button>Thêm khóa học</Button>
-        </Link>
+        <DataTable columns={columns} data={data} />
       </div>
     );
 }
