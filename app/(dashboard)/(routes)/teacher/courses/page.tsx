f2224fe64f26b6fee2f5bf3ad402cdc91d18f167
xpbachx <21020283@vnu.edu.vn>
2023-11-06 15:06:50 +0700

    [QLKHGV_VIEW] Table query to show course

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -2,25 +2,29 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { DataTable } from "./_components/data-table";
 import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-async function getData(): Promise<any[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
-}
 
 const CoursesPage = async() =>{
-    const data = await getData();
+    const {userId} = auth();
+    if (!userId) {
+      return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      }
+    })
+
     return (
       <div className="h-full flex justify-center pt-20">
-        <DataTable columns={columns} data={data} />
+        <DataTable columns={columns} data={courses} />
       </div>
     );
 }
