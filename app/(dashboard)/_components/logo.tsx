44c0781c3f51013371ac6faff5d84922c60d786d
hongquant17 <hongquant.1712@gmail.com>
2023-12-07 17:48:34 +0700

    [GENERAL] logo change

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,7 +1,12 @@
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Logo = () => {
+  const { theme, setTheme } = useTheme();
+  const isDarkMode = theme === 'dark'
+  const logoSrc = isDarkMode ? '/light_logo.svg' : '/logo.svg';
+
   return (
     <Link href="/">
       <Image
@@ -10,7 +15,7 @@ export const Logo = () => {
         alt="logo"
         loading="eager"
         priority={true}
-        src="/logo.svg"
+        src={logoSrc}
       />
     </Link>
   );
