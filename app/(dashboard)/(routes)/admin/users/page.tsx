c4a1a6ec3025c69eb72c3ce1331980bb4ca65166
hongquant17 <hongquant.1712@gmail.com>
2023-12-07 21:16:29 +0700

    [QLND_CLASSIFY] restructure folder

--- app/(dashboard)/(routes)/admin/users/page.tsx
+++ app/(dashboard)/(routes)/admin/users/page.tsx
@@ -1,8 +1,8 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
-import { DataTable } from "../_components/data-table";
-import { columns } from "../_components/columns";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
 const UserPage = async () => {
   const { userId } = auth();
