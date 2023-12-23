3c0e11528b4a5a8f232d21ff2c7167cde07cbb4e
xpbachx <21020283@vnu.edu.vn>
2023-12-23 23:16:16 +0700

    [AUTHORIZATION] Protect route for teacher & remove clerk env

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth";
-import { isAdminSession } from "@/lib/admin";
 import { isTeacherSession } from "@/lib/teacher";
 
 const TeacherLayout = async ({
@@ -10,7 +9,7 @@ const TeacherLayout = async ({
 }) => {
     const session = await getSession();
 
-    if (!isAdminSession(session?.user.role) && !isTeacherSession(session?.user.role)) {
+    if (!isTeacherSession(session?.user.role)) {
         return redirect("/");
     }
     return <>{children}</>
