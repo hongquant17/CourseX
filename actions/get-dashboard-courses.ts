bfa67dc9085230d06c2563dfe894e2e1a0e77a4c
hongquant17 <hongquant.1712@gmail.com>
2023-12-28 00:39:33 +0700

    [QLKHHS_VIEW] fix view only accepted course

--- actions/get-dashboard-courses.ts
+++ actions/get-dashboard-courses.ts
@@ -4,56 +4,64 @@ import { Category, Chapter, Course } from "@prisma/client";
 import { getProgress } from "@/actions/get-progress";
 
 type CourseWithProgressWithCategory = Course & {
-    category: Category;
-    chapters: Chapter[];
-    progress: number | null;
+  category: Category;
+  chapters: Chapter[];
+  progress: number | null;
 };
 
 type DashboardCourses = {
-    completedCourses: CourseWithProgressWithCategory[];
-    coursesInProgress: CourseWithProgressWithCategory[];
-}
+  completedCourses: CourseWithProgressWithCategory[];
+  coursesInProgress: CourseWithProgressWithCategory[];
+};
 
-
-export const getDashboardCourses = async (userId: string): Promise<DashboardCourses> => {
-    try {
-        const enrolledCourses = await db.enroll.findMany({
-            where: {
-                userId: userId,
+export const getDashboardCourses = async (
+  userId: string
+): Promise<DashboardCourses> => {
+  try {
+    const enrolledCourses = await db.enroll.findMany({
+      where: {
+        userId: userId,
+        isAccepted: true,
+      },
+      select: {
+        course: {
+          include: {
+            category: true,
+            chapters: {
+              where: {
+                isPublished: true,
+              },
             },
-            select: {
-                course: {
-                    include: {
-                        category: true,
-                        chapters: {
-                            where: {
-                                isPublished: true,
-                            }
-                        }
-                    }
-                }
-            }
-        });
+          },
+        },
+      },
+    });
 
-        const courses = enrolledCourses.map((enroll) => enroll.course) as CourseWithProgressWithCategory[];
+    const courses = enrolledCourses.map(
+      (enroll) => enroll.course
+    ) as CourseWithProgressWithCategory[];
 
-        for (let course of courses) {
-            const progress = await getProgress(userId, course.id);
-            course["progress"] = progress;
-        }
-
-        const completedCourses = courses.filter((course) => course.progress === 100);
-        const coursesInProgress = courses.filter((course) => (course.progress ?? 0) < 100);
-
-        return {
-            completedCourses,
-            coursesInProgress,
-        }
-    } catch (error) {
-        console.log("[GET_DASHBOARD_COURSES", error);
-        return {
-            completedCourses: [],
-            coursesInProgress: [],
-        }
+    for (let course of courses) {
+      const progress = await getProgress(userId, course.id);
+      course["progress"] = progress;
     }
+
+    const completedCourses = courses.filter(
+      (course) => course.progress === 100
+    );
+    const coursesInProgress = courses.filter(
+      (course) => (course.progress ?? 0) < 100
+    );
+
+    return {
+      completedCourses,
+      coursesInProgress,
+    };
+  } catch (error) {
+    console.log("[GET_DASHBOARD_COURSES", error);
+    return {
+      completedCourses: [],
+      coursesInProgress: [],
+    };
+  }
 };
