e6bbde0abaa3dbf8a51bf5e665a2d43d1fbfe17c
xpbachx <21020283@vnu.edu.vn>
2023-12-28 00:32:41 +0700

    [PROFILE] Fix theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
     <SessionProvider session={session} >
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
-          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
             <ConfettiProvider />
             <ToasterProvider />
             {children}
