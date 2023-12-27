786b4ef4b20ead6ff4ec2fa659ab3164336e05e8
xpbach2508 <21020283@vnu.edu.vn>
2023-12-27 15:58:42 +0700

    [FORUM] Like & unlike

--- app/api/courses/[courseId]/comment/like/route.ts
+++ app/api/courses/[courseId]/comment/like/route.ts
@@ -16,6 +16,25 @@ export async function POST(
         const body = await req.json();
         const userIdLike = body.userId;
         const commentIdLike = body.commentId;
+        const existedLike = await db.like.findFirst({
+            where: {
+                userId: userIdLike,
+                commentId: commentIdLike,
+            }
+        })
+        if (existedLike) {
+            const deletedLike = await db.like.delete({
+                where: {
+                    userId_commentId: {
+                        userId: userIdLike,
+                        commentId: commentIdLike
+                    }
+                }
+            })
+            if (deletedLike) {
+                return new NextResponse("Deleted", {status: 200});
+            }
+        }
         const newLiked = await db.like.create({
             data: {
                 userId: userIdLike,
