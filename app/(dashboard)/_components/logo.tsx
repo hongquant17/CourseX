242938fd8417ea418ad5152a43b72dd8161da660
xpbachx <21020283@vnu.edu.vn>
2024-01-01 17:30:56 +0700

    [THEME] Fix system mode conflict with server

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,22 +1,8 @@
-import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Logo = () => {
-  const { theme, setTheme } = useTheme();
-  const [logoSrc, setLogo] = useState(theme === "dark" ? "/light_logo.svg" : "/logo.svg");
-  useEffect(() => {
-    if (theme === "system" && typeof window !== "undefined") {
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        setLogo("/light_logo.svg");
-      } else {
-        setLogo("/logo.svg");
-      }
-    } else {
-      setLogo(theme === "dark" ? "/light_logo.svg" : "/logo.svg");
-    }
-  }, [logoSrc, theme]);
+  const logoSrc = "/logo.svg";
 
   return (
     <Link href="/">
