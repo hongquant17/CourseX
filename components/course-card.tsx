87fc2b98d5fba3e8dbe8110d401f3fd80ceede1c
hongquant17 <hongquant.1712@gmail.com>
2024-01-06 23:01:42 +0700

    [GENERAL] fix hydration failed

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -26,49 +26,43 @@ export const CourseCard = ({
   categories,
 }: CourseCardProps) => {
   return (
-    <>
-      <Link href={`/search/overview/${id}`}>
-        <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-          <div className="relative w-full aspect-video rounded-md overflow-hidden">
-            <Image fill alt={title} src={imageUrl} />
-          </div>
-          <div className="flex flex-col pt-2">
-            <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
-              {title}
-            </div>
-            <div className="text-xs text-muted-foreground">
-              {categories.map((category) => (
-                <Link
-                  key={category.id}
-                  href={`/search?categoryId=${category.id}`}
-                >
-                  <Badge className={cn("bg-white text-black-500")}>
-                    {category.name}
-                  </Badge>
-                </Link>
-              ))}
-            </div>
-            <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-              <div className="flex items-center gap-x-1 text-slate-500">
-                <IconBadge size="sm" icon={BookOpen} />
-                <span>
-                  {chaptersLength}{" "}
-                  {chaptersLength === 1 ? "Chapter" : "Chapters"}
-                </span>
-              </div>
-            </div>
-            {progress !== null ? (
-              <CourseProgress
-                variant={progress === 100 ? "success" : "default"}
-                size="sm"
-                value={progress}
-              />
-            ) : (
-              <p className="text-md md:text-sm font-medium text-slate-700"></p>
-            )}
+    <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+      <div className="relative w-full aspect-video rounded-md overflow-hidden">
+        <Link href={`search/overview/${id}`}>
+          <Image fill alt={title} src={imageUrl} />
+        </Link>
+      </div>
+      <div className="flex flex-col pt-2">
+        <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+          <Link href={`search/overview/${id}`}>{title}</Link>
+        </div>
+        <div className="text-xs text-muted-foreground">
+          {categories.map((category) => (
+            <Link key={category.id} href={`/search?categoryId=${category.id}`}>
+              <Badge className={cn("bg-white text-black-500")}>
+                {category.name}
+              </Badge>
+            </Link>
+          ))}
+        </div>
+        <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+          <div className="flex items-center gap-x-1 text-slate-500">
+            <IconBadge size="sm" icon={BookOpen} />
+            <span>
+              {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+            </span>
           </div>
         </div>
-      </Link>
-    </>
+        {progress !== null ? (
+          <CourseProgress
+            variant={progress === 100 ? "success" : "default"}
+            size="sm"
+            value={progress}
+          />
+        ) : (
+          <p className="text-md md:text-sm font-medium text-slate-700"></p>
+        )}
+      </div>
+    </div>
   );
 };
